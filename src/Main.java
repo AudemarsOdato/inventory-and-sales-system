cb5c65f829212b2b6b404c972cc4d32da9bac575
Audemars Odato <audemarsodato@gmail.com>
2025-12-17 13:56:16 +0800

    add: encryption utilities

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import database.Users;
+import utilities.JCrypt;
 
 public class Main {
         // start command: java -classpath ".\lib\postgresql-42.2.29.jre7.jar" .\src\Main.java
         public static void main(String[] args) {
-                Users user = new Users();
+                JCrypt jcrypt = new JCrypt();
 
-                System.out.println(user.insert("owner", "audemars odato", "2386br8237b23", 123));
+                System.out.println(jcrypt.hash("trisha"));
+                System.out.println(jcrypt.isMatch("trisha", "c721f0c3e3ed6a1bc6c76deda4bd38e2da768971da2879513c69e541b529382e"));
         }
 }
--- src/utilities/JCrypt.java
+++ src/utilities/JCrypt.java
@@ -1,12 +1,37 @@
 package utilities;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+// must be used only in the forntend
 public class JCrypt {
-        // creating salt
-
-
+        // generating salt
         // adding salt to hashed password
         // hash password
+        public String hash(String password) {
+                try {
+                        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                        byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 
+                        StringBuilder hexString = new StringBuilder();
+                        for (byte bit : hashBytes) {
+                                String hex = Integer.toHexString(0xff & bit);
+                                if (hex.length() == 1) {
+                                        hexString.append('0');
+                                }
+                                hexString.append(hex);
+                        }
+                        return hexString.toString();
+                }
+                catch (NoSuchAlgorithmException error) {
+                        error.printStackTrace();
+                }
 
-        // comparing hashed password
+                return "";
+        }
+
+        public boolean isMatch(String password, String hashed) {
+                return hashed.equals(hash(password));
+        }
 }
